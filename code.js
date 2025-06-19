ff8150aad3b43528d89bc068fb1328b9ad754836
Mauricio123A <105246738+Mauricio123A@users.noreply.github.com>
2025-06-19 20:00:59 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+let pixelScale = 20 // px
+let initialSnakeScale = 3 // +head
+let snakeColors = ["rgb(70, 247, 138)","rgb(229, 247, 70)","rgb(112, 1, 139)","rgb(91, 214, 19)","rgb(70, 247, 232)","rgb(243, 19, 19)","rgb(247, 2, 226)","rgb(86, 94, 91)","rgb(201, 90, 0)","rgb(37, 28, 122)"]
+let tickTime = 500 // miliseconds
+
+
+function drawSnakeHead(){
+    
+}
